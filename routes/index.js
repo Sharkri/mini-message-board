06d835357122bb708f24d710e42cf853714d533a
Sharkri <sharkdude955@gmail.com>
2023-04-21 05:33:00 -0500

    Fix time not being local

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const isToday = require("date-fns/isToday");
 
 const router = express.Router();
 
-function getFormattedDate(date = new Date()) {
+function convertDateToLocal(date) {
+  return new Date(new Date(date).toLocaleString());
+}
+
+function getFormattedDate(date) {
   if (isToday(date)) return `Today at ${format(date, "h:mm a")}`;
 
   return format(date, `MMM dd,${isThisYear(date) ? "" : " yyyy"} h:mm a`);
@@ -15,13 +19,24 @@ const messages = [];
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Mini Message Board", messages });
+  const localTimeMessages = messages.map((msg) => ({
+    ...msg,
+    date: {
+      ...msg.date,
+      string: getFormattedDate(convertDateToLocal(msg.date.added)),
+    },
+  }));
+
+  res.render("index", {
+    title: "Mini Message Board",
+    messages: localTimeMessages,
+  });
 });
 
 router.post("/new", (req, res, next) => {
   const { name, message } = req.body;
 
-  const currentDate = new Date();
+  const currentDate = Date.now();
 
   if (name && message) {
     messages.push({
@@ -29,7 +44,6 @@ router.post("/new", (req, res, next) => {
       text: message,
       date: {
         added: currentDate,
-        string: getFormattedDate(currentDate),
       },
     });
   }
